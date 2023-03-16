9b33fe9b52a8df0f1b8858f71e4cb35fc79a0834
Alfredo.Herrera <ing.alfredoherrera@hotmail.com>
2023-03-16 10:20:53 -0600

    fix: example proxy

--- src/components/FloatButton/index.tsx
+++ src/components/FloatButton/index.tsx
@@ -1,29 +1,25 @@
-import SocialDistanceIcon from '@mui/icons-material/SocialDistance'; import Fab from '@mui/material/Fab';
+import SocialDistanceIcon from '@mui/icons-material/SocialDistance';
+import Fab from '@mui/material/Fab';
 import Script from 'next/script';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const FloatButton = () => {
-    const [viewScript, SetViewScript] = useState(false)
-    useEffect(() => {
+  const [viewScript, SetViewScript] = useState(false);
 
-    }, [])
-
-    return (
+  return (
+    <>
+      {viewScript && (
         <>
-            {
-                viewScript && (
-                    <>
-                        <Script src="https://apps.elfsight.com/p/platform.js" />
-                        <div className="elfsight-app-4f8ae513-2eab-4fd2-966d-49828a7d5e6c" />
-                    </>
-                )
-            }
-
-            <Fab aria-label="edit" onClick={() => SetViewScript(true)}>
-                <SocialDistanceIcon />
-            </Fab>
+          <Script src="https://apps.elfsight.com/p/platform.js" />
+          <div className="elfsight-app-4f8ae513-2eab-4fd2-966d-49828a7d5e6c" />
         </>
-    )
-}
+      )}
 
-export default FloatButton
+      <Fab aria-label="edit" onClick={() => SetViewScript(true)}>
+        <SocialDistanceIcon />
+      </Fab>
+    </>
+  );
+};
+
+export default FloatButton;
